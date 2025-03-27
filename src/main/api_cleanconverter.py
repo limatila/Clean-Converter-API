4b31803029427a1fa2db3ef28a16bd5056f6c0a3
Átila <atilalimade@gmail.com>
2025-03-27 13:54:38 -0300

    api: redirecting download path to a local one and also handling any subprocess exception (shall be replaced later) tested and working with requests!

--- src/main/api_cleanconverter.py
+++ src/main/api_cleanconverter.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from middleware.conversion import *
-from services.youtubeDownloaders import *
+from src.middleware.conversion import *
+from src.services.youtubeDownloaders import *
 
 app = FastAPI(version="0.1", title="Clean Converter API",
               description="Mp3 downloader only, for now.")
 
-@app.get(f"/{app.version}" + "/download/mp3/{url_inserted}")
-def get_video_in_mp3(url_inserted: str):
-    downloadOutputPath = download_raw_audio(url_inserted)
+@app.get(f"/v{app.version}" + "/download/mp3/")
+def get_video_in_mp3(url: str):
+    downloadOutputPath = download_raw_audio(url)
     locationMp3 = convert_to_mp3(downloadOutputPath)
+
+    if not locationMp3:
+        return HTTPException(status_code=500, detail="Could not resolve video conversion.")
     
     # success
-    print("Video has been successfully saved, in .mp3: " + str(locationMp3))
+    return {
+        "result": "Video has been successfully saved, in .mp3",
+        "location": locationMp3
+    }
--- src/middleware/conversion.py
+++ src/middleware/conversion.py
@@ -1,21 +1,28 @@
 from pathlib import Path
 from subprocess import run, PIPE
 
-#! Need to change behavior: will not be just served locally, but also for api, so need to use another temp/ folder for conversions
-def convert_to_mp3(input_path) -> Path: #Reliable.
+def convert_to_mp3(input_path: str) -> Path: #Reliable.
     print("INFO: converting to MP3...")
 
-    downloadsFolderPath = Path.home() / "Downloads"
-    output_file = downloadsFolderPath / (Path(input_path).stem + ".mp3")
+    conversionFolderPath = Path("./temp-converted")
+    if not conversionFolderPath.is_dir():
+        conversionFolderPath.mkdir()
 
+    newConvertedFilePath = (Path(input_path).stem + ".mp3")
+    output_file = conversionFolderPath / newConvertedFilePath
+
+    #TODO: implement ffmpeg-python binding libr
     command = [
-        "ffmpeg", "-y",  # Overwrite without asking
-        "-i", str(input_path),  # Input file
-        "-vn",  # Remove video streams if present
+        "ffmpeg", "-y",           # Overwrite without asking
+        "-i", str(input_path),    # Input file
+        "-vn",                    # Remove video streams if present
         "-acodec", "libmp3lame",  # Use LAME MP3 encoder
-        "-q:a", "2",  # High-quality variable bitrate (lower is better, range: 0-9)
-        str(output_file)  # Output file
+        "-q:a", "2",              # High-quality variable bitrate (lower is better, range: 0-9)
+        str(output_file)          # Output file
     ]
-    run(command, stdout=PIPE, stderr=PIPE)
+    processConversion = run(command, stdout=PIPE, stderr=PIPE)
 
-    return output_file
+    if processConversion.returncode == 0:
+        return output_file
+    else:
+        raise Exception(f"Audio conversion failed on subprocess: {processConversion.stderr.decode()}")
