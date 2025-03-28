7833c82bfe4bb49758735e3b4059b09f4fa865c7
Átila <atilalimade@gmail.com>
2025-03-28 16:45:05 -0300

    api: added FileResponse to return a mp3 file from local dir
    adding TODOs.txt file for diclosure of what's next

--- src/main/api_cleanconverter.py
+++ src/main/api_cleanconverter.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
 
 from src.middleware.conversion import *
 from src.services.youtubeDownloaders import *
@@ -7,15 +8,26 @@ app = FastAPI(version="0.1", title="Clean Converter API",
               description="Mp3 downloader only, for now.")
 
 @app.get(f"/v{app.version}" + "/download/mp3/")
-def get_video_in_mp3(url: str):
+async def get_video_in_mp3(url: str):
+
+    #TODO: apply condition for "mp3 or mp4 already exists" and imeddiatly returns. #! should result in a race condition, so need to account for that also!
     downloadOutputPath = download_raw_audio(url)
     locationMp3 = convert_to_mp3(downloadOutputPath)
+    filenameMp3 = str(locationMp3.stem) + ".mp3"
 
-    if not locationMp3:
-        return HTTPException(status_code=500, detail="Could not resolve video conversion.")
-    
     # success
-    return {
-        "result": "Video has been successfully saved, in .mp3",
-        "location": locationMp3
-    }
+    if locationMp3:
+        return FileResponse(
+            filename=filenameMp3,
+            path=locationMp3,
+            media_type="audio/mpeg",
+            headers={
+                "result": "Video has been successfully converted to .mp3"
+            }
+        )
+        # return {
+        #     "result": "Video has been successfully saved, in .mp3",
+        #     "location": locationMp3
+        # }
+    else:
+        return HTTPException(status_code=500, detail="Could not resolve video conversion.")
--- src/services/youtubeDownloaders.py
+++ src/services/youtubeDownloaders.py
@@ -1,15 +1,19 @@
+from pathlib import Path
+
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
 def download_raw_audio(url: str):
     yt_Client = YouTube(url, on_progress_callback=on_progress)
-    tempDownloadsFolder = "./temp-downloads/" #path to temp files
+    downloadsFolderPath = Path("./temp-downloads/") #path to temp files
+    if not downloadsFolderPath.is_dir():
+        downloadsFolderPath.mkdir()
 
     if yt_Client: 
         print("INFO: Video found: " + yt_Client.title)
 
     yt_AudioStream = yt_Client.streams.get_audio_only()
-    downloadedFilePath = yt_AudioStream.download(tempDownloadsFolder)
+    downloadedFilePath = yt_AudioStream.download(downloadsFolderPath)
     if downloadedFilePath: 
         print("INFO: m4a downloaded.")
 
