21870bd0de9014e16e53768b1bf5f05840586ae5
Átila <atilalimade@gmail.com>
2025-03-29 01:40:05 -0300

    refactor: conversion adaptor is now runned with ffmpeg binding
    ffmpeg-python should be used for any future conversion functions
    added to requirements.txt

--- src/middleware/conversion.py
+++ src/middleware/conversion.py
@@ -1,28 +1,34 @@
 from pathlib import Path
-from subprocess import run, PIPE
+
+import ffmpeg #bindings libr, needs ffmpeg installed and in PATH
 
 def convert_to_mp3(input_path: str) -> Path: #Reliable.
-    print("INFO: converting to MP3...")
-
     conversionFolderPath = Path("./temp-converted")
     if not conversionFolderPath.is_dir():
         conversionFolderPath.mkdir()
 
     newConvertedFilePath = (Path(input_path).stem + ".mp3")
-    output_file = conversionFolderPath / newConvertedFilePath
+    output_path = conversionFolderPath / newConvertedFilePath
 
-    #TODO: implement ffmpeg-python binding libr
-    command = [
-        "ffmpeg", "-y",           # Overwrite without asking
-        "-i", str(input_path),    # Input file
-        "-vn",                    # Remove video streams if present
-        "-acodec", "libmp3lame",  # Use LAME MP3 encoder
-        "-q:a", "2",              # High-quality variable bitrate (lower is better, range: 0-9)
-        str(output_file)          # Output file
-    ]
-    processConversion = run(command, stdout=PIPE, stderr=PIPE)
+    #TODO: add condition if file exists
 
-    if processConversion.returncode == 0:
-        return output_file
+    print("INFO: converting to MP3...")
+    conversionProcess = (
+        ffmpeg.input(
+                input_path, 
+                format="m4a"
+            ).output(
+                str(output_path),
+                vn= None, # -vn option, remove video frames
+                acodec="libmp3lame", 
+                aq= "2", # -q:a option with alias, lower number, better quality 
+            ).overwrite_output() # -y option
+            .run_async(pipe_stdout=True, pipe_stderr=True)
+    )
+    out, err = conversionProcess.communicate() 
+
+    if conversionProcess.returncode == 0:
+        return output_path
     else:
-        raise Exception(f"Audio conversion failed on subprocess: {processConversion.stderr.decode()}")
+        print("-------Last Logs:\n", out.decode())
+        raise Exception(f"Audio conversion failed on subprocess: {err.decode()}")
