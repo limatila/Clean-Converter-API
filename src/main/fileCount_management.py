e1f265649ab9880b6599611443a56115b0148c5f
Átila <atilalimade@gmail.com>
2025-03-29 22:08:52 -0300

    api fix: added more rules for usage accounting and corrected bugs
    tested and accepting requests
    will add new video names to usage-reg.json and reset to 0 when deleting files

--- src/main/fileCount_management.py
+++ src/main/fileCount_management.py
@@ -26,6 +26,8 @@ except KeyError:
         dump(usage_reg, usage_json, indent=USAGE_REG_INDENT)
     totalExecutionCount: int = 0
 
+
+#* Management functions
 def delete_stored_files(filename: str, fileExtension: str, converted: bool = True):
     toDelete: list[Path] = []
     defaultFileExtension_downloads = ".m4a"
@@ -33,17 +35,17 @@ def delete_stored_files(filename: str, fileExtension: str, converted: bool = Tru
     #getting and checking files
     #TODO: log assertion errors
     file_downloaded: Path = Path("./temp-downloads") / (filename + defaultFileExtension_downloads)
-    assert file_downloaded.exists() == True
     toDelete.append(file_downloaded)
 
     if converted: #False if you don't want to delete the .mp3 converted file
         file_converted: Path = Path("./temp-converted") / (filename + fileExtension)
-        assert file_converted.exists() == True
         toDelete.append(file_converted)
 
     #deleting them
     for file in toDelete:
-        file.unlink()
+        file.unlink(missing_ok=True)   #TODO: log FileNotFound error
+    #resetting count on file
+    update_usage_registry({filename: 0})
 
 def update_usage_registry(entry: dict[str, int]):
     """Function to update var 'usage_reg' & file on path 'USAGE_REG_FILENAME' 
@@ -51,8 +53,8 @@ def update_usage_registry(entry: dict[str, int]):
     Args:
         entry (dict[str, int]): a dict with 'filename' or 'USAGE_REG_EXECUTIONS_KEY' as key, and a number on its value
     """
+    usage_reg.update(entry)
     with open(USAGE_REG_FILENAME, 'w') as usage_json: #also update it
-        usage_reg.update(entry)
         dump(usage_reg, usage_json, indent=USAGE_REG_INDENT)
         
 def account_for_usage(files_paths: list[Path]):
@@ -60,6 +62,7 @@ def account_for_usage(files_paths: list[Path]):
 
     file_downloaded, file_converted = files_paths
     filename = str(file_converted.stem)
+    fileExtension = str(file_converted.suffix)
 
     # add count to entry #? should do this on if/else ?
     try:
@@ -68,7 +71,7 @@ def account_for_usage(files_paths: list[Path]):
         usage_reg.update({str(filename): 0}) #starts at 0 usage
         countFileUsage: int = usage_reg[ filename ]
     
-    countFileUsage += 1
+    countFileUsage += 1     #TODO: this cant be permanent. Must account for days since last usage
 
     # add entry update to usage_reg
     update_usage_registry({filename: countFileUsage})
@@ -77,8 +80,8 @@ def account_for_usage(files_paths: list[Path]):
     totalExecutionCount += 1
     update_usage_registry({USAGE_REG_EXECUTIONS_KEY: totalExecutionCount})
     
-    # call delete_stored_files() if file is not so much used
-    if (totalExecutionCount % 20 == 0
-        and totalExecutionCount != 0 #avoid bug in first run of .json
-        and countFileUsage <= 4 ):
-        delete_stored_files(filename, ".mp3")
+    # call delete_stored_files() if file is not so much used, every 20th request (max 20x +-10MB music)
+    if ( (totalExecutionCount % 20) == 0 and totalExecutionCount != 0 ):
+        for name in usage_reg:
+            if (usage_reg[name] <= 4 and name != filename and name != USAGE_REG_EXECUTIONS_KEY):
+                delete_stored_files(name, fileExtension)
