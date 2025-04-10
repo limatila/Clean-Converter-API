413617be478ea4f67c2ea5145653cb2cf698f4e8
Átila <atilalimade@gmail.com>
2025-04-10 15:24:10 -0300

    services: added local cookiefile validation
    procedure should be:
    extract from browser manually into 'cookie.txt' and 'backup-cookie.txt';
    $ 'chmod 444 backup-cookie.txt'
    then every time a request is sent, cookie.txt is updated with the backup automatically.

--- src/main/api_youtubeconverter.py
+++ src/main/api_youtubeconverter.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI, HTTPException, BackgroundTasks
 from fastapi.responses import FileResponse
 
 from src.services import inputValidation
+from src.services.cookieValidation import validate_cookies
 from src.services.youtubeDownloaders import download_mp3
 from src.main.fileCount_management import account_for_usage
 
 app = FastAPI(version="1.1", title="Youtube Clean Converter",
               description=(
                     "## An API for automatic download and conversion of Youtube videos.\n Mp3 downloader only, for now.\n" +
-                    "## NOTE:\n you should note that this API doesn't have a HTTPS certificate, but use it in mind that IT SHOULD ONLY download MP3 files.") 
+                    "## NOTE:\n you should note that this API doesn't have a HTTPS certificate, but use it in mind that IT SHOULD ONLY download MP3 files."), 
              )
 
 @app.get(f"/v{app.version}" + "/download/mp3/", description="Downloads given URL's audio, in browser. Just put a video url in the input to download it (ETA: 20s)")
@@ -24,6 +25,7 @@ def get_in_mp3(url: str, background: BackgroundTasks):
         filenameMp3 = downloadOutputPath.name
         #file usage management
         background.add_task(account_for_usage, downloadOutputPath)
+        background.add_task(validate_cookies)
 
         return FileResponse(
             filename=filenameMp3,
