21a41462999b6c57625d4304c8f2e8f4fe0fac49
Átila <atilalimade@gmail.com>
2025-04-10 23:58:18 -0300

    config: changing ydl_opts.cookiefile variable

--- src/config.py
+++ src/config.py
@@ -1,10 +1,14 @@
 #Configurations for the project (mainly: names and paths)
 
 from pathlib import Path
-from dotenv import load_dotenv as load_envfile  #TODO
 
-#Downloaders
+#Cookies for youtube client
+COOKIES_FILE_PATH = "cookies.txt"
+COOKIES_BACKUP_FILE_PATH = "backup-cookies.txt"
+
+#Downloads configuration
 DOWNLOADS_FOLDER_PATH = Path("./temp-downloads")
+
 YDL_OPTS: dict[str, any] = {
         'format': 'bestaudio/best',
         'outtmpl': str(DOWNLOADS_FOLDER_PATH / '%(title)s'),
@@ -13,16 +17,12 @@ YDL_OPTS: dict[str, any] = {
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'cookiefile': 'cookies.txt',  # Path to your cookies file
+        'cookiefile': COOKIES_FILE_PATH,  # Path to your cookies file
         'overwrites': False,
         'noplaylist': True,
         'age_limit': 20,
         'wait_for_video': (0, 86400) #For lives that didn't finish (but will), will wait for 24hours for it to finish.
 }
-
-#Cookies for youtube client
-COOKIES_FILE_PATH = "cookies.txt"
-COOKIES_BACKUP_FILE_PATH = "backup-cookies.txt"
 
 #File usage registry
 USAGE_REG_PATH = "./src/logs/usage-reg.json"
